5c03c411ca34d08873b4c3f312ffe63cda44a500
PartierSP <mikevl1973@gmail.com>
2021-01-11 04:48:16 -0500

    Added basic functions

--- ArDNC.ino
+++ ArDNC.ino
@@ -1,1 +1,14 @@
-// ArDNC
+#include <Wire.h>
+#include <SD.h>
+#include <Adafruit_RGBLCDShield.h>
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
